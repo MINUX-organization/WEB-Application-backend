1fb1b5686b190495bd08e9cba20acd8f593a66bf
Unusualy1 <syzuki45@gmail.com>
2023-10-01 21:15:08 +0700

    hot fix dynamicdata calc

--- src/temp/dynamic.js
+++ src/temp/dynamic.js
@@ -8,75 +8,88 @@ const dynamicData = {
     rams : null,
     get calculations() {
         // Variables
-        const result = {
-            coinsValue: null,
-            totalSharesAccepted: 0,
-            totalSharesRejected: 0,
-            workingAlgorithms: 0,
-            workingMiner: 0,
-            totalPower: 0,
-            totalRam: 0
-        }
-        const miners = []
-        const algorithms = []
-        const coins = []
-        // GPU 
-        this.gpus.forEach(gpu => {
-            staticData.gpus.forEach(gpuStatic => {
-                if (gpuStatic.uuid === gpu.uuid) {
-                    gpu.fullName = `${gpuStatic.information.manufacturer} ${gpuStatic.information.periphery}`
+        const calculateTotalSharesAccepted = () => {
+            let total = 0;
+            if (this.gpus) {
+                for (const gpu of this.gpus) {
+                    if (gpu.shares.accepted) {
+                        total += gpu.shares.accepted
+                    }
                 }
-            })
-            result.totalSharesAccepted += gpu.shares.accepted
-            result.totalSharesRejected += gpu.shares.rejected
-            result.totalPower += gpu.powerUsage
-            if (gpu.miner) {
-                miners.push(gpu.miner.uuid)
             }
-            if (gpu.algorithms) {
-                algorithms.push(gpu.algorithm)
+            return total;
+        } 
+        const calculateTotalSharesRejected = () => {
+            let total = 0;
+            if (this.gpus) {
+                for (const gpu of this.gpus) {
+                    if (gpu.shares.rejected) {
+                        total += gpu.shares.rejected
+                    }
+                }
             }
-            if (gpu.cryptocurrency && gpu.algorithm) {
-                coins.push({coin: gpu.cryptocurrency, algorithm: gpu.algorithm})
-            }
-        });
-        // CPU
-        result.totalSharesAccepted += this.cpu.shares.accepted
-        result.totalSharesRejected += this.cpu.shares.rejected
-        result.totalPower += this.cpu.powerUsage
-        if (this.cpu.miner) {
-            miners.push(this.cpu.miner.uuid)
+            return total;
         }
-        if (this.cpu.algorithm) {
-            algorithms.push(this.cpu.algorithm)
-        }
-        if (this.cpu.cryptocurrency && this.cpu.cryptocurrency) {
-            coins.push({coin: this.cpu.cryptocurrency, algorithm: this.cpu.algorithm})
-        }
-        // RAM
-        this.rams.forEach(element => {
-            result.totalRam += element.usage.value
-        })
-        // Sort arrays with miners/algorithms
-        result.workingAlgorithms = [...new Set(algorithms)].length
-        result.workingMiner = [...new Set(miners)].length
-        // Coins
-        const coinCount = coins.reduce((acc, item) => {
-            if (!acc[item.coin]) {
-                acc[item.coin] = { coin: item.coin, algorithm: item.algorithm, value: 1 };
-            } else {
-                acc[item.coin].value++
+        const calculateWorkingAlgorithms = () => {
+            const algorithms = [];
+            if (this.gpus) {
+                for (const gpu of this.gpus) {
+                    if (gpu.algorithm) {
+                        algorithms.push(gpu.algorithm)
+                    }
+                }
             }
-            return acc;
-        }, {});
-        result.coinsValue = Object.values(coinCount).map((item) => {
-            return { coin: item.coin, algorithm: item.algorithm, value: item.value }
-        });
-        console.timeLog(coins)
-        console.log(coinCount)
-        console.log(result.coinsValue)
-        // Return result
-        return result
+            if (this.cpu && this.cpu.algorithm) {
+                algorithms.add(this.cpu.algorithm)
+            }
+            return algorithms.length
+        }
+        const calculateWorkingMiners = () => {
+            const miners = [];
+            if (this.gpus) {
+                for (const gpu of this.gpus) {
+                    if (gpu.miner) {
+                        miners.push(gpu.miner)
+                    }
+                }
+            }
+            if (this.cpu && this.cpu.miner) {
+                miners.add(this.cpu.miner)
+            }
+            return miners.length
+        }
+        const calculateTotalPower = () => {
+            let total = 0;
+            if (this.gpus) {
+                for (const gpu of this.gpus) {
+                    if (gpu.powerUsage) {
+                        total += gpu.powerUsage;
+                    }
+                }
+            }
+            if (this.cpu && this.cpu.powerUsage) {
+                total += this.cpu.powerUsage;
+            }
+        }
+        const calculateTotalRam = () => {
+            let total = 0;
+            if (this.rams) {
+                for (const ram of this.rams) {
+                    if (ram.usage && ram.usage.value) {
+                        total += ram.usage.value
+                    }
+                }
+            }
+        }
+        return {
+            coinsValue: null,
+            totalSharesAccepted: calculateTotalSharesAccepted(),
+            totalSharesRejected: calculateTotalSharesRejected(),
+            workingAlgorithms: calculateWorkingAlgorithms(),
+            workingMiners: calculateWorkingMiners(),
+            totalPower: calculateTotalPower(),
+            totalRam: calculateTotalRam()
+        }
     }
 }
 
